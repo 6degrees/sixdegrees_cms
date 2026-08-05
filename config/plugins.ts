c9b96cb0b041bbfd55c0bf002a9b7b77c288ce19
Fatima Al-Ibrahim <fatimaalibrahim010@gmail.com>
2026-08-05 10:25:34 +0300

    Fix16

--- config/plugins.ts
+++ config/plugins.ts
@@ -45,16 +45,6 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   },
   upload: {
     config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
       sizeLimit: 500 * 1024 * 1024, // 500MB
       security: {
         allowedTypes: allowedMediaTypes,
