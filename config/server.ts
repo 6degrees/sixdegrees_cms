88ddc5ffae38ce8ff027f19e4ecf1a4cc8a9125f
abdroos <abd.alaidrous@gmail.com>
2026-08-11 22:26:28 +0300

    update nginx

--- config/server.ts
+++ config/server.ts
@@ -1,18 +1,15 @@
-import type { Core } from '@strapi/strapi';
+import type {Core} from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+const config = ({env}: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     url: env('PUBLIC_URL'),
     app: {
         keys: env.array('APP_KEYS')!,
     },
-    webhooks: {
-        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
     transfer: {
         remote: {
-            assetIdleTimeoutMs: env.int('TRANSFER_ASSET_IDLE_TIMEOUT_MS', 6000000),
+            enabled: true,
         },
     },
 });
