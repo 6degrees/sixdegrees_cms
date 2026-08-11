18deba037b07454df99299f6fcba1dff425c2892
abdroos <abd.alaidrous@gmail.com>
2026-08-11 16:31:56 +0300

    remove default value

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,15 @@
-import type {Core} from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
 
-const config = ({env}: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    url: env('PUBLIC_URL', 'https://strapi.6d.com.sa'),
-    app: {
-        keys: env.array('APP_KEYS')!,
-    },
-    webhooks: {
-        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL'),
+  app: {
+    keys: env.array('APP_KEYS')!,
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
 
 export default config;
