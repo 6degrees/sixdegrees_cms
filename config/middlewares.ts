4d097c60f8ed246d3840c08f4c08a2a0337663ac
abdroos <abd.alaidrous@gmail.com>
2026-08-11 23:16:05 +0300

    update s3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,71 +1,73 @@
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'res.cloudinary.com',
-          ],
-          upgradeInsecureRequests: null,
+    'strapi::logger',
+    'strapi::errors',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': ["'self'", 'https:'],
+                    'img-src': [
+                        "'self'",
+                        'data:',
+                        'blob:',
+                        'market-assets.strapi.io',
+                        'res.cloudinary.com',
+                        'hel1.your-objectstorage.com',
+                    ],
+                    'media-src': [
+                        "'self'",
+                        'data:',
+                        'blob:',
+                        'res.cloudinary.com',
+                        'hel1.your-objectstorage.com',
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
         },
-      },
     },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'http://localhost:3003',
-        'https://burooj.pro',
-        'https://buroojair.com',
-        'https://naqsh.com',
-        'https://6degrees.com.sa',
-        'https://efficiencys.com.sa',
-        'https://naqsh-holding.github.io',
-        'https://burooj-pro.github.io',
-        'https://efficiency-center.github.io',
-        'https://6degrees.github.io',
-      ],
-      headers: '*',
-      credentials: true,
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: [
+                'http://localhost:3000',
+                'http://localhost:3001',
+                'http://localhost:3002',
+                'http://localhost:3003',
+                'https://burooj.pro',
+                'https://buroojair.com',
+                'https://naqsh.com',
+                'https://6degrees.com.sa',
+                'https://efficiencys.com.sa',
+                'https://naqsh-holding.github.io',
+                'https://burooj-pro.github.io',
+                'https://efficiency-center.github.io',
+                'https://6degrees.github.io',
+            ],
+            headers: '*',
+            credentials: true,
+        },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '500mb',
-      jsonLimit: '500mb',
-      textLimit: '500mb',
-      formidable: {
-        maxFileSize: 500 * 1024 * 1024, // 500MB
-      },
+    'strapi::poweredBy',
+    'strapi::query',
+    {
+        name: 'strapi::body',
+        config: {
+            formLimit: '500mb',
+            jsonLimit: '500mb',
+            textLimit: '500mb',
+            formidable: {
+                maxFileSize: 500 * 1024 * 1024, // 500MB
+            },
+        },
     },
-  },
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
 ];
 
 export default config;
